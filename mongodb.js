ae1bb92bd579fa378bfb99471b6eac9e7a1d8939
Dreez <Hedrees05@gmail.com>
2022-07-15 18:25:49 -0700

    Init commit

--- mongodb.js
+++ mongodb.js
@@ -6,7 +6,7 @@
 // Using destructuring to access properties of mongodb
 const {MongoClient, ObjectID} = require('mongodb')
 
-const connectionURL = 'mongodb://127.0.0.1:27017'
+const connectionURL = process.env.MONGODB_URL
 const databaseName = 'task-manager'
 
 // const id = new ObjectID()
